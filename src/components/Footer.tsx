0ea544458e57867e4c1a9a026fe00047df002103
jovenjuanpi <juanpi@artu.ai>
2024-08-20 11:00:09 -0600

    badge

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 
 export const Footer = () => {
-  return (  
+  return (
     <footer id="footer">
       <hr className="w-11/12 mx-auto" />
       <section className="pb-14 text-center">
         <h3>
-          &copy; Artu. Todos los derechos reservados | 646-321-0085 | juanpi@artu.ai 
-          <a
-            rel="noreferrer noopener"
-            target="_blank"
-            className="text-primary transition-all border-primary hover:border-b-2"
-          >
-          </a>
+          &copy; Artu. Todos los derechos reservados | 646-321-0085 | juanpi@artu.ai
         </h3>
+        <div className="flex justify-center mt-4">
+          <a href="https://www.aitechsuite.com/tools/22311?ref=featured&v=146" target="_blank" rel="nofollow">
+            <img
+              width="150"
+              src="https://aitsmarketing.s3.amazonaws.com/aits-verified-tool.svg?width=600"
+              alt="Verified Tool on AI Tech Suite"
+            />
+          </a>
+        </div>
       </section>
     </footer>
   );
